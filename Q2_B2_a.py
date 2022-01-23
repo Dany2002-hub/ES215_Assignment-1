b00baa16bc6b9aba9d709b3e0fcb3bc7cf502857
Dany2002-hub <76448256+Dany2002-hub@users.noreply.github.com>
2022-01-24 01:29:23 +0530

    Create Q2_B2_a.py

---
+++ Q2_B2_a.py
@@ -0,0 +1,15 @@
+import numpy as np
+import time
+ 
+n=int(input("Enter size of matrix: "))
+
+mat1=np.random.randint(low=1,high=32767,size=(n,n))/7
+mat2=np.random.randint(low=1,high=32767,size=(n,n))/7
+
+start=time.time_ns()
+mat3 = np.dot(mat1,mat2)
+end=time.time_ns()
+
+print(mat3)
+
+print("Time taken for matrix multiplication", (end-start)*1e-9)
